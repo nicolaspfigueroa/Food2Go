b01668bd0c43ce25074ee89a78daec9af1473e2e
unknown <nicolaspfigueroa@gmail.com>
2022-06-24 15:45:36 -0600

    added models to sb

---
+++ server/models/dishes.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => sequelize.define('Dishes', {
+  restaurantId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Restaurants",
+        key: "id",
+        //deferrable: Deferrable.INITIALLY_IMMEDIATE
+      }
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  }
+});
---
+++ server/models/restaurants.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => sequelize.define('Restaurants', {
+  name: {
+    type: DataTypes.STRING
+  },
+  lat: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  long: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  imgUrl: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
