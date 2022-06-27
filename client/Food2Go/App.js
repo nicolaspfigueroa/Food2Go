26b244a0ceef8a09856885caae1b89a79a408c39
unknown <nicolaspfigueroa@gmail.com>
2022-06-27 15:52:08 -0600

    adding authentication

--- client/Food2Go/App.js
+++ client/Food2Go/App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import auth from './utils/auth';
 
 export default function App() {
+
+  const initialState = auth.isAuthenticated();
+  const [isAuthenticated, setIsAuthenticated] = useState(initialState);
+
   return (
     <View style={styles.container}>
       <Text>Is working</Text>
