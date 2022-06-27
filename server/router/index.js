081af8fc533dd1077b6e7bf743a732683dec1d37
unknown <nicolaspfigueroa@gmail.com>
2022-06-27 12:24:40 -0600

    added dish backend functions

--- server/router/index.js
+++ server/router/index.js
@@ -2,11 +2,13 @@ const { Router } =  require('express');
 const {userRouter} =  require('./userRouter');
 const {restaurantRouter} = require('./restaurantRouter');
 const {stripeRouter} = require('./stripeRouter');
+const {dishRouter} = require('./dishRouter');
 
 const rootRouter = Router();
 
 rootRouter.use('/user', userRouter);
 rootRouter.use('/restaurant', restaurantRouter);
 rootRouter.use('/payment', stripeRouter);
+rootRouter.use('./dish', dishRouter);
 
 exports.router = rootRouter;
