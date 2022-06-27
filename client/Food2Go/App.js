db0a6f1b796ae8ad7255cb53b925c6c81889f2da
PeicoPaco <rodrigo.reategui.g@gmail.com>
2022-06-27 17:13:40 -0500

    router working

--- client/Food2Go/App.js
+++ client/Food2Go/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+
+import ListView from "./screens/ListView";
+import Login from "./screens/Login";
+import MapView from "./screens/MapView";
+import Menu from "./screens/Menu";
+import Profile from "./screens/Profile";
+import Register from "./screens/Register";
+import ShopCart from "./screens/ShopCart";
+
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme, 
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent"
+  }
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Is working</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme = {theme}>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="ListView"  component={ListView} />
+        <Stack.Screen name="MapView"  component={MapView} />
+        <Stack.Screen name="Menu" component={Menu} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="ShopCart" component={ShopCart} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
