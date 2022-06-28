48b0f3d06df4471be9ade63ebd3365a3f4c53d16
unknown <nicolaspfigueroa@gmail.com>
2022-06-27 18:23:50 -0600

    working register

--- client/Food2Go/screens/Register.js
+++ client/Food2Go/screens/Register.js
@@ -1,8 +1,26 @@
 //no auth required 
 import { View, Text } from 'react-native'
 import React from 'react'
+import auth from '../utils/auth';
+import apiServiceJWT from './../ApiServiceJWT';
 
-const Register = () => {
+const initialState = {
+  email: '',
+  password: '',
+  nickname: '',
+};
+
+const Register = (props) => {
+  const [state, setState] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <View>
       <Text>Register</Text>
