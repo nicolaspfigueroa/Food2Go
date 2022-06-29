5b34253e9f67785a8d39d5cc10510da0cac2aef8
Carlos <carlospinill@gmail.com>
2022-06-29 14:39:05 -0500

    more styling

--- client/Food2Go/screens/Menu.js
+++ client/Food2Go/screens/Menu.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Image, SafeAreaView } from 'react-native'
+import { StyleSheet, View, Text, Image, SafeAreaView, TouchableWithoutFeedback } from 'react-native'
 import React from 'react'
 import { DishService } from '../services/DishService';
 
@@ -36,13 +36,12 @@ const Menu = ( {route, navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-       <Text onPress={()=>navigation.goBack()} style={{paddingTop:100}}>Go back </Text>
-
+      <TouchableWithoutFeedback onPress={()=>navigation.goBack()}><Image source={require("./goback.png")} style={{width:50,height:50}} /> 
+      </TouchableWithoutFeedback>
+       <Image source={require("./logo.png")} style={{width:50,height:50, marginRight:10}} />
       </View>
       <View style={styles.tittle}>
-        <Text>
-          {route.params.restaurant.name}
-        </Text>
+      <Image source={require('./14-inkas.png')} style={{width:230,height:70}}/>
       </View>
       <View style={styles.menucontainer}>
           {dishes.map((dish) => (
@@ -60,30 +59,38 @@ const Menu = ( {route, navigation}) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+   
   },
   header: {
     flex: 1,
     backgroundColor: '#38b000',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    height :'20%',
+    justifyContent: 'space-between',
+    paddingTop:10,
+    height :10,
     width: '100%',
   },
   tittle: {
-    width: 200,
-    height: 150,
+    flex :1,
+    width: 300,
+    height: 165,
+    alignItems: 'center',
+    justifyContent: 'center',
+
   },
   menucontainer: {
-    width: 150,
-    height: 250,
+    width: '100%',
+    height: 350,
   },
   dish: {
-    width: 50,
-    height: 50,
+    width: 100,
+    height: 100,
+    borderStyle : 'solid',
   },
   dishdetail: {
     width: 150,
