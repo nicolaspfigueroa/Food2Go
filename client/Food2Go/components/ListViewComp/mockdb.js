954ce40cddb9fd39e0ed40f490e501d5260044f0
PeicoPaco <rodrigo.reategui.g@gmail.com>
2022-06-29 12:28:28 -0500

    images rendering but weird text

--- client/Food2Go/components/ListViewComp/mockdb.js
+++ client/Food2Go/components/ListViewComp/mockdb.js
@@ -1,13 +1,30 @@
-[{"name": "Coctel del Mar", "latitude": 4.70362, "longitude": -74.04885,
-"city":"Bogota","hours":"",
-"description": "Seafood restaurant", "imageURL": "../assets/coctel-del-mar.png",
-"address": "Av. Cra 19 #123-26"},
-{"name": "14 Inkas", "latitude": 4.69903, "longitude": -74.02959,
-    "city":"Bogota","hours":"",
-    "description": "Peruvian restaurant", "imageURL": "../assets/14-inkas.png",
-    "address": "Cl. 119b #543"},
-{"name": "Barricas tapas y Canas", "latitude": 4.69894, "longitude": -74.03861,
-    "city":"Bogota","hours":"",
-    "description": "Spanish restaurant", "imageURL": "../assets/barricas-tapas-y-canas.png",
-    "address": "Cl. 119 #11d-15"}
+import assets from "../../constants/assets";
+
+const RestaurantData = [
+    {
+        id : "Restaurant-01",
+        name: "Coctel del Mar",
+        latitude: 4.70362,
+        longitude: -74.04885,
+        city: "Bogota",
+        image: assets.restaurant01
+    },
+    {
+        id : "Restaurant-02",
+        name: "14 Inkas",
+        latitude: 4.70362,
+        longitude: -74.04885,
+        city: "Bogota",
+        image: assets.restaurant02
+    },
+    {
+        id : "Restaurant-03",
+        name: "Barricas tapas y canas",
+        latitude: 4.70362,
+        longitude: -74.04885,
+        city: "Bogota",
+        image: assets.restaurant03
+    },
 ]
+
+export {RestaurantData}
