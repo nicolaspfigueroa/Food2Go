daccaf21167e68a7d6b57477a9969ef927f8870f
unknown <nicolaspfigueroa@gmail.com>
2022-07-01 14:15:13 -0600

    implemented shopping cart checkout, need to test

--- client/Food2Go/screens/ShopCart.js
+++ client/Food2Go/screens/ShopCart.js
@@ -1,13 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import NavTop from '../components/NavTop/NavTop';
 import NavBottom from '../components/NavBottom/NavBottom';
+import CartItem from '../components/Cart/CartItem';
+import stripeService from '../services/StripeService';
 
 const ShopCart = ({ cart, setCart }) => {
+
+  const handleDeleteCart = (id) => {
+    console.log(id);
+    console.log(cart);
+    setCart((prevValue) => {
+      const allButId = prevValue.filter((dish) => dish.id !== id);
+      return allButId;
+    });
+  };
+
   return (
     <View>
       <NavTop></NavTop>
       <Text>ShopCart</Text>
+      {cart.map((dish) => {
+        <CartItem key = {dish.id} dish = {dish} handleDeleteCart = {handleDeleteCart} ></CartItem>
+      })}
+      <TouchableOpacity onPress={() => stripeService.createCheckoutSession(cart)}>
+        <Text>Checkout</Text>
+      </TouchableOpacity>
       <NavBottom></NavBottom>
     </View>
   )
