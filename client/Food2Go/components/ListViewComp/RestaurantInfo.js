2a98be72eee5b4981141c2f52a64b7bd79427118
PeicoPaco <rodrigo.reategui.g@gmail.com>
2022-07-02 14:20:43 -0500

    working on route

--- client/Food2Go/components/ListViewComp/RestaurantInfo.js
+++ client/Food2Go/components/ListViewComp/RestaurantInfo.js
@@ -1,22 +1,29 @@
-import { View, Text, Image, SafeAreaView } from 'react-native'
+import { View, Text, Image, SafeAreaView, TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import {RestaurantDescription} from './RestaurantDescription'
 import listViewStyles from '../../constants/styles/ListViewStyles'
-import React from 'react'
+
 
 const RestaurantInfo = ({data}) => {
+  console.log(data.image)
+  const navigation = useNavigation();
   return (
     <SafeAreaView style={{flex: 1}}>
+      <Text>{data.id} {data.name}</Text>
+      <TouchableOpacity onPress={() => {navigation.navigate("Menu", {data} )}}>
       <View style={listViewStyles.restaurantlistcontainer}>
         <View style={listViewStyles.imagecontainer}>
           <Image
-            source={data.image}
-            resizeMode = "cover"
+            source={{uri: 'https://media-cdn.tripadvisor.com/media/photo-s/1c/05/db/e7/barricas-tapas-canas.jpg'
+          }}
+            //'../../assets/images/restaurants/barricas-tapas-y-canas.png'
+            resizeMode = 'contain'
             style= {listViewStyles.image}
           />
         </View>  
         <RestaurantDescription title = {data.name}></RestaurantDescription>
       </View>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
