df5255b344794283b65532576d2b40067d4ae2f7
PeicoPaco <rodrigo.reategui.g@gmail.com>
2022-07-02 15:05:17 -0500

    routes working

--- client/Food2Go/components/ListViewComp/RestaurantInfo.js
+++ client/Food2Go/components/ListViewComp/RestaurantInfo.js
@@ -1,28 +1,27 @@
-import { View, Text, Image, SafeAreaView, TouchableOpacity } from 'react-native'
+import { View, Image, SafeAreaView, TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import {RestaurantDescription} from './RestaurantDescription'
 import listViewStyles from '../../constants/styles/ListViewStyles'
 
 
-const RestaurantInfo = ({data}) => {
-  console.log(data.image)
+const RestaurantInfo = ({restaurant}) => {
   const navigation = useNavigation();
+  //
   return (
     <SafeAreaView style={{flex: 1}}>
-      <Text>{data.id} {data.name}</Text>
-      <TouchableOpacity onPress={() => {navigation.navigate("Menu", {data} )}}>
-      <View style={listViewStyles.restaurantlistcontainer}>
-        <View style={listViewStyles.imagecontainer}>
-          <Image
-            source={{uri: 'https://media-cdn.tripadvisor.com/media/photo-s/1c/05/db/e7/barricas-tapas-canas.jpg'
-          }}
-            //'../../assets/images/restaurants/barricas-tapas-y-canas.png'
-            resizeMode = 'contain'
-            style= {listViewStyles.image}
-          />
-        </View>  
-        <RestaurantDescription title = {data.name}></RestaurantDescription>
-      </View>
+      <TouchableOpacity onPress={() => {navigation.navigate("Menu", {restaurant} )}}>
+        <View style={listViewStyles.restaurantlistcontainer}>
+          <View style={listViewStyles.imagecontainer}>
+            <Image
+              source={{uri: 'https://media-cdn.tripadvisor.com/media/photo-s/1c/05/db/e7/barricas-tapas-canas.jpg'
+            }}
+              //'../../assets/images/restaurants/barricas-tapas-y-canas.png'
+              resizeMode = 'contain'
+              style= {listViewStyles.image}
+            />
+          </View>  
+          <RestaurantDescription title = {restaurant.name}></RestaurantDescription>
+        </View>
       </TouchableOpacity>
     </SafeAreaView>
   )
