77c8e2dd88b09dc0ca6794e5782ff96fd432e531
unknown <nicolaspfigueroa@gmail.com>
2022-07-04 16:23:13 -0600

    cart number implemented with delete

--- client/Food2Go/components/NavBottom/NavBottom.js
+++ client/Food2Go/components/NavBottom/NavBottom.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
 
 import icons from '../../constants/icons'
@@ -10,14 +10,36 @@ const NavBottom = () => {
 
   const navigation = useNavigation();
 
-  const { numInCart } =useContext(CartContext);
+  //const { numInCart } =useContext(CartContext);
+  const {cart, setCart} = useContext(CartContext); 
+  const [quantity, setQuantity] = useState('');
+
+  useEffect(() => {
+    //let totalPrice = [100];
+    setQuantity(getTotalQuantity(cart));
+    
+  }, [cart]);
+
+
+  const getTotalQuantity = (cart) => {
+    let total = 0;
+    if (cart.length === 0) {
+      return '';
+    }
+    else {
+      total = cart.reduce((a,b) => {
+        return a + b.quantity;
+      }, 0)
+    }
+    return total;
+  }
 
   return (
     <View style={styles.bottom}>
       <TouchableOpacity onPress = {() => navigation.navigate('ListView')}>        
             <Image source={icons.list} style={{width:35,height:35, marginLeft:15}} />
       </TouchableOpacity>
-      {numInCart>0 &&<Image style ={styles.num} source={icons[`n${numInCart}`]}/>}
+      {quantity>0 &&<Image style ={styles.num} source={icons[`n${quantity}`]}/>}
       <TouchableOpacity onPress = {() => navigation.navigate('ShopCart')}>  
         
         <Image source={icons.cart} style={{width:66,height:66, marginBottom:15}} />
