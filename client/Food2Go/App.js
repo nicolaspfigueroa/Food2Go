f35d2731ecb439cf54a7ac1054091c51f4e5cd09
unknown <nicolaspfigueroa@gmail.com>
2022-07-05 07:54:35 -0600

    changed initial route to login

--- client/Food2Go/App.js
+++ client/Food2Go/App.js
@@ -50,7 +50,7 @@ export default function App() {
       <NavigationContainer theme = {theme}>
         
 
-            <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Register
+            <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Login
             ">
               <Stack.Screen name="Login" >
                 {props => <Login setIsAuthenticated = {setIsAuthenticated}/>}
